4b1923ce8de471d729ea299c45a06b096470af2d
Jelle Meijer <jellejanwil@gmail.com>
2020-02-24 19:06:45 +0100

    removed config error

--- roam_boter/roamboter/settings.py
+++ roam_boter/roamboter/settings.py
@@ -120,10 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
