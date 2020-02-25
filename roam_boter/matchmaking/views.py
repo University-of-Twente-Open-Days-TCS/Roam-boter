0f10b81584d32d638877fe82f063e334830c397c
Jelle Meijer <jellejanwil@gmail.com>
2020-02-25 18:06:13 +0100

    Work on creating api

--- roam_boter/matchmaking/views.py
+++ roam_boter/matchmaking/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.views import View
+
 # Create your views here.
 class SaveAIView(View):
 
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             # process AI 
-            pass
+            return HttpResponse('just text')
         else:
-            response = HttpResponse()
+           return HttpResponse('Unauthorized')
 
+    def get(self, request, *args, **kwargs):
+        print("Hello AI VIEW!")
+        return HttpResponse('<html><body>hello</body></html>')
