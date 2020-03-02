02eea8d3cefb4b7116d0ddf7a872f76382446292
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-02 14:24:04 +0100

    Added fullscreen support and non-functional navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,108 +1,41 @@
 // frontend/src/App.js
 
-import React, { Component } from "react";
-const todoItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    description: "Buy ingredients to prepare dinner",
-    completed: true
-  },
-  {
-    id: 2,
-    title: "Study",
-    description: "Read Algebra and History textbook for upcoming test",
-    completed: false
-  },
-  {
-    id: 3,
-    title: "Sally's books",
-    description: "Go to library to rent sally's books",
-    completed: true
-  },
-  {
-    id: 4,
-    title: "Article",
-    description: "Write article on how to use django with react",
-    completed: false
-  }
-];
+import React, {Component} from "react";
+import AppContent from "./components/AppContent";
+import Fullscreen from "react-full-screen";
+import Button from "@material-ui/core/Button";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewCompleted: false,
-      todoList: todoItems
-    };
-  }
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
+    constructor(props) {
+        super();
+
+        this.state = {
+            isFull: false,
+        };
     }
-    return this.setState({ viewCompleted: false });
-  };
-  renderTabList = () => {
-    return (
-        <div className="my-5 tab-list">
-            <span
-                onClick={() => this.displayCompleted(true)}
-                className={this.state.viewCompleted ? "active" : ""}
-            >
-              complete
-            </span>
-          <span
-              onClick={() => this.displayCompleted(false)}
-              className={this.state.viewCompleted ? "" : "active"}
-          >
-              Incomplete
-            </span>
-        </div>
-    );
-  };
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-        item => item.completed == viewCompleted
-    );
-    return newItems.map(item => (
-        <li
-            key={item.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-        >
-            <span
-                className={`todo-title mr-2 ${
-                    this.state.viewCompleted ? "completed-todo" : ""
-                }`}
-                title={item.description}
-            >
-              {item.title}
-            </span>
-          <span>
-              <button className="btn btn-secondary mr-2"> Edit </button>
-              <button className="btn btn-danger">Delete </button>
-            </span>
-        </li>
-    ));
-  };
-  render() {
-    return (
-        <main className="content">
-          <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-          <div className="row ">
-            <div className="col-md-6 col-sm-10 mx-auto p-0">
-              <div className="card p-3">
-                <div className="">
-                  <button className="btn btn-primary">Add task</button>
-                </div>
-                {this.renderTabList()}
-                <ul className="list-group list-group-flush">
-                  {this.renderItems()}
-                </ul>
-              </div>
+
+    goFull = () => {
+        this.setState({isFull: true});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>This app only works fullscreen, please click below to enter the app!</h1>
+                <Button onClick={this.goFull} margin-left="auto" margin-right="auto">
+                    Go Fullscreen
+                </Button>
+                <Fullscreen
+                    enabled={this.state.isFull}
+                    onChange={isFull => this.setState({isFull})}
+                >
+                    <div className="full-screenable-node">
+                        {this.state.isFull ? <AppContent/> : null}
+                    </div>
+                </Fullscreen>
             </div>
-          </div>
-        </main>
-    );
-  }
+        );
+    }
 }
+
 export default App;
