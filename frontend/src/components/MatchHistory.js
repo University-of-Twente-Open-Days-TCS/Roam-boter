a27c0666aa5b0bf4737d64f7c859630da93ddd32
Jelle Meijer <jellejanwil@gmail.com>
2020-03-09 21:15:08 +0100

    list of matches

--- frontend/src/components/MatchHistory.js
+++ frontend/src/components/MatchHistory.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
 
-const MatchHistory = () => {
-    return (
+import { getCsrfToken } from '../utils.js'
+
+
+
+class MatchHistory extends Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {matches: []}
+    }
+
+    async componentDidMount() {
+        let data = await this.getMatchHistory()
+        let new_state = {matches: data}
+        this.setState(new_state)
+    }
+
+    async getMatchHistory() {
+        let csrfToken = await getCsrfToken()
+        const response = await fetch('http://localhost:8000/matches/botmatches/',
+        {
+            'method' : 'GET',
+            'credentials': 'include'
+        })
+        let data = await response.json()
+        return data
+    }
+
+    render() {
+        return(
         <div>
             <h1>Match History</h1>
-            <p>Match History will be visible here.</p>
+            { 
+                this.state.matches.map((match, i) => {
+                    return (<div key={i}>AI {match.ai} : DATE {match.date} <button>Watch Simulation</button></div>)
+                })
+            }
         </div>
-    )
+        )
+    }
 }
 
 export default MatchHistory;
