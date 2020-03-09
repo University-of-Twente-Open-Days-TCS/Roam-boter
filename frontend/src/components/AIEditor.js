a880ae30e5bb36e31ea04dd9e6ac1c82eaf68ac9
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-09 13:39:53 +0100

    Rewrote AIEditor component class to function

--- frontend/src/components/AIEditor.js
+++ frontend/src/components/AIEditor.js
@@ -1,14 +1,12 @@
 import React, {Component} from "react";
 
-class AIEditor extends Component {
-    render() {
-        return (
-            <div>
-                <h1>AI Editor</h1>
-                The AI Editor will be placed here.
-            </div>
-        );
-    }
+const AIEditor = () => {
+    return (
+        <div>
+            <h1>AI Editor</h1>
+            The AI Editor will be placed here.
+        </div>
+    );
 }
 
-export default AIEditor;
+export default AIEditor
