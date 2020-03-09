485bd76fecdfe68e4c21979f022af739bbdf2dba
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-09 15:22:29 +0100

    Removed unused imports

--- frontend/src/components/ListAIs.js
+++ frontend/src/components/ListAIs.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const ListAIs = () => {
+const ListAIs = ({AIs}) => {
     return (
         <div>
             <h1>List of AIs</h1>
             A list of the team's AIs will be visible here.
+            <ul>
+                {AIs.map((ai, i) =>
+                    <li key={i}>{ai.name}</li>
+                )}
+            </ul>
         </div>
     );
 }
