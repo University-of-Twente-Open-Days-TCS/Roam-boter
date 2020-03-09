b0e2e58933f07e23c3fcde4ca2a680c13b095342
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-09 14:41:53 +0100

    Made frontend a little more readable

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,19 +1,13 @@
 import React, {useState} from "react"
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import '../css/Login.css';
 
 const Login = ({handleSubmit}) => {
-    const [teamCode, setTeamCode] = useState("")
-
-    // const handleSubmit = (evt) => {
-    //     evt.preventDefault()
-    //     console.log(teamCode)
-    // }
+    const [teamCode, setTeamCode] = useState(156077)
 
     return (
-        <div>
-            <form className="test" noValidate autoComplete="off" onSubmit={() => handleSubmit(teamCode)}>
-                <label>Team code</label>
+        <div className="login-form">
+            <form className="login-form" noValidate autoComplete="off" onSubmit={() => handleSubmit(teamCode)}>
+                <label>Team code </label>
                 <input type="text" data-test="team-code" value={teamCode} onChange={e => setTeamCode(e.target.value)}/>
 
                 <input type="submit" value="Log In" data-test="submit"/>
