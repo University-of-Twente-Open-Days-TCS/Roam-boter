dcc7ef488420b6ea19d7e072cc91f90aa0d7fa68
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-12 13:19:43 +0100

    Revamped login screen and added feedback on entering wrong team code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
             isFull: false,
             AIs: [],
             loggedIn: false,
+            loginAttemptFailed: false
         };
     }
 
@@ -45,7 +46,7 @@ class App extends Component {
         let response = RoamBotAPI.loginUser(teamCode)
         response
             .then((response) => {
-                response.ok ? this.setState({loggedIn: true}) : this.setState({loggedIn: false})
+                response.ok ? this.setState({loggedIn: true, loginAttemptFailed: false}) : this.setState({loggedIn: false, loginAttemptFailed: true})
             })
     }
 
@@ -87,7 +88,7 @@ class App extends Component {
                             </Layout>
                         </div>
                     </Fullscreen>
-                </div>) : (<Login handleSubmit={this.handleSubmitLogin.bind(this)}/>)
+                </div>) : (<Login handleSubmit={this.handleSubmitLogin.bind(this)} attemptFailed={this.state.loginAttemptFailed}/>)
         );
     }
 }
--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,21 +1,45 @@
 import React, {useState} from "react"
 import '../css/Login.css';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
-const Login = ({handleSubmit}) => {
-    const [teamCode, setTeamCode] = useState(156077)
+const Login = ({handleSubmit, attemptFailed}) => {
+    const [teamCode, setTeamCode] = useState("")
 
     return (
-        <div className="login-form">
-            <form className="login-form" noValidate autoComplete="off" onSubmit={(event) => { 
-                    event.preventDefault()
-                    handleSubmit(teamCode)
-                    }
-                }>
-                <label>Team code </label>
-                <input type="text" data-test="team-code" value={teamCode} onChange={e => setTeamCode(e.target.value)}/>
+        <div>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
 
-                <input type="submit" value="Log In" data-test="submit"/>
-            </form>
+                <Grid item xs={3}>
+                    <form className="login-form" noValidate autoComplete="off" onSubmit={(event) => {
+                        event.preventDefault()
+                        handleSubmit(teamCode)
+                    }
+                    }>
+                        <TextField className="form-input"
+                                   id="outlined-basic"
+                                   label="Team code"
+                                   variant="outlined"
+                                   value={teamCode} onChange={e => setTeamCode(e.target.value)}
+                                   helperText={attemptFailed ? "This team code doesn't exist" : ""}
+                                   error={attemptFailed} />
+                        <Button className="form-input" variant="contained" color="primary" type="submit">
+                            Log In
+                        </Button>
+                    </form>
+                </Grid>
+
+            </Grid>
+
         </div>
     )
 }
