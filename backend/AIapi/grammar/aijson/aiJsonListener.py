c5c80c6db38f475f315fac774f39a3304b9a188e
Jelle Meijer <jellejanwil@gmail.com>
2020-03-12 14:50:40 +0100

    Regenerated aiJson grammar files

---
+++ backend/AIapi/grammar/aijson/aiJsonListener.py
@@ -0,0 +1,163 @@
+# Generated from aiJson.g4 by ANTLR 4.7.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .aiJsonParser import aiJsonParser
+else:
+    from aiJsonParser import aiJsonParser
+
+# This class defines a complete listener for a parse tree produced by aiJsonParser.
+class aiJsonListener(ParseTreeListener):
+
+    # Enter a parse tree produced by aiJsonParser#startrule.
+    def enterStartrule(self, ctx:aiJsonParser.StartruleContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#startrule.
+    def exitStartrule(self, ctx:aiJsonParser.StartruleContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#node.
+    def enterNode(self, ctx:aiJsonParser.NodeContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#node.
+    def exitNode(self, ctx:aiJsonParser.NodeContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#condition.
+    def enterCondition(self, ctx:aiJsonParser.ConditionContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#condition.
+    def exitCondition(self, ctx:aiJsonParser.ConditionContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#conditiondata.
+    def enterConditiondata(self, ctx:aiJsonParser.ConditiondataContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#conditiondata.
+    def exitConditiondata(self, ctx:aiJsonParser.ConditiondataContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#childtrue.
+    def enterChildtrue(self, ctx:aiJsonParser.ChildtrueContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#childtrue.
+    def exitChildtrue(self, ctx:aiJsonParser.ChildtrueContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#childfalse.
+    def enterChildfalse(self, ctx:aiJsonParser.ChildfalseContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#childfalse.
+    def exitChildfalse(self, ctx:aiJsonParser.ChildfalseContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#actionblock.
+    def enterActionblock(self, ctx:aiJsonParser.ActionblockContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#actionblock.
+    def exitActionblock(self, ctx:aiJsonParser.ActionblockContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#actiondata.
+    def enterActiondata(self, ctx:aiJsonParser.ActiondataContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#actiondata.
+    def exitActiondata(self, ctx:aiJsonParser.ActiondataContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#actionlist.
+    def enterActionlist(self, ctx:aiJsonParser.ActionlistContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#actionlist.
+    def exitActionlist(self, ctx:aiJsonParser.ActionlistContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#action.
+    def enterAction(self, ctx:aiJsonParser.ActionContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#action.
+    def exitAction(self, ctx:aiJsonParser.ActionContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#attributes.
+    def enterAttributes(self, ctx:aiJsonParser.AttributesContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#attributes.
+    def exitAttributes(self, ctx:aiJsonParser.AttributesContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#attributevalues.
+    def enterAttributevalues(self, ctx:aiJsonParser.AttributevaluesContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#attributevalues.
+    def exitAttributevalues(self, ctx:aiJsonParser.AttributevaluesContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#attributevalue.
+    def enterAttributevalue(self, ctx:aiJsonParser.AttributevalueContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#attributevalue.
+    def exitAttributevalue(self, ctx:aiJsonParser.AttributevalueContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#value.
+    def enterValue(self, ctx:aiJsonParser.ValueContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#value.
+    def exitValue(self, ctx:aiJsonParser.ValueContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#position.
+    def enterPosition(self, ctx:aiJsonParser.PositionContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#position.
+    def exitPosition(self, ctx:aiJsonParser.PositionContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#positiondata.
+    def enterPositiondata(self, ctx:aiJsonParser.PositiondataContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#positiondata.
+    def exitPositiondata(self, ctx:aiJsonParser.PositiondataContext):
+        pass
+
+
+    # Enter a parse tree produced by aiJsonParser#type_id.
+    def enterType_id(self, ctx:aiJsonParser.Type_idContext):
+        pass
+
+    # Exit a parse tree produced by aiJsonParser#type_id.
+    def exitType_id(self, ctx:aiJsonParser.Type_idContext):
+        pass
+
+
