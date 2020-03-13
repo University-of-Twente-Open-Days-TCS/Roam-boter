797e0438fd67f1e5b87980ce8a5756d19db7fdf0
Jelle Meijer <jellejanwil@gmail.com>
2020-03-13 15:02:28 +0100

    improved login button layout

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,11 +1,28 @@
 import React, {useState} from "react"
-import '../css/Login.css';
+
+import { makeStyles } from '@material-ui/core/styles'
+
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+
+
+const useStyles = makeStyles(theme => ({
+    loginForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+
+        padding: '1rem',
+    },
+    formInput: {
+        margin: '1rem',
+        width: '100%'
+    }
+}))
 
 const Login = ({handleSubmit, attemptFailed}) => {
+    const classes = useStyles()
     const [teamCode, setTeamCode] = useState("")
 
     return (
@@ -19,20 +36,20 @@ const Login = ({handleSubmit, attemptFailed}) => {
                 style={{ minHeight: '100vh' }}
             >
 
-                <Grid item xs={3}>
-                    <form className="login-form" noValidate autoComplete="off" onSubmit={(event) => {
+                <Grid item sm={9} md={3}>
+                    <form className={classes.loginForm} noValidate autoComplete="off" onSubmit={(event) => {
                         event.preventDefault()
                         handleSubmit(teamCode)
                     }
                     }>
-                        <TextField className="form-input"
+                        <TextField className={classes.formInput}
                                    id="outlined-basic"
                                    label="Team code"
                                    variant="outlined"
                                    value={teamCode} onChange={e => setTeamCode(e.target.value)}
                                    helperText={attemptFailed ? "This team code doesn't exist" : ""}
                                    error={attemptFailed} />
-                        <Button className="form-input" variant="contained" color="primary" type="submit">
+                        <Button className={classes.formInput} variant="contained" color="primary" type="submit">
                             Log In
                         </Button>
                     </form>
