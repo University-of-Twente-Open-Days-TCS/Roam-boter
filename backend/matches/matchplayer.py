f25e970721c0391af0c749b1b059128b3cccbce0
Daan Middelkoop <daantje676@gmail.com>
2020-03-22 19:24:10 +0100

    Add workers for simulations

--- backend/matches/matchplayer.py
+++ backend/matches/matchplayer.py
@@ -1,45 +1,52 @@
 from simulation.simulation import simulate
+from workers import task
+from matches.models import BotMatch
 
 from AIapi.grammar import converter
 
 import logging
+
 logger = logging.getLogger("debugLogger")
 
 
-class SimulationPlayer:
+@task()
+def process_simulation_task(botmatch_id):
 
+    botmatch = BotMatch.objects.get(pk=botmatch_id)
+
+    if botmatch.simulation.state == "DONE":
+        raise Exception("Simulation already has run")
+
+    bot = botmatch.bot
+    bot_ai = bot.ais.first()  # take first of all ais
+    team_ai = botmatch.ai
+
+    # convert ais to AINode objects
+    bot_ai_eval_tree = converter.convert_aijson(bot_ai.ai)
+    team_ai_eval_tree = converter.convert_aijson(team_ai.ai)
+
+    playback = simulate([team_ai_eval_tree, bot_ai_eval_tree])
+
+    if playback.winner == 0:
+        botmatch.winner = botmatch.team
+    else:
+        botmatch.winner = None
+
+    botmatch.simulation.simulation = playback.to_json(botmatch.team_id, -1)
+    botmatch.simulation.state = "DONE"
+    botmatch.save()
+    logger.debug("Saved the match")
+
+
+class SimulationPlayer(object):
     def run_botmatch(self, botmatch):
         """
         Expects a pending BotMatch instance and runs the simulation.
         """
-        if botmatch.simulation.state == "DONE":
-            raise Exception("Simulation already has run")
-
-        bot = botmatch.bot
-        bot_ai = bot.ais.first() # take first of all ais
-        team_ai = botmatch.ai
-
-        # convert ais to AINode objects
-        bot_ai_eval_tree = converter.convert_aijson(bot_ai.ai)
-        team_ai_eval_tree = converter.convert_aijson(team_ai.ai)
 
         # send team ai as first player
-        playback = simulate([team_ai_eval_tree, bot_ai_eval_tree])
-        # set winner
-        winner = playback.winner
 
-        if winner == 0:
-            botmatch.winner = botmatch.team
-        else:
-            botmatch.winner = None
-
-        botmatch.simulation.simulation = playback.to_json(botmatch.team_id, -1)
-        botmatch.simulation.state = "DONE"
-        botmatch.simulation.save()
-        botmatch.save()
-
-
-
+        process_simulation_task(botmatch.pk)
 
 
 # Setup simulation player object
--- backend/roamboter/settings.py
+++ backend/roamboter/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'fontawesome_5',
+    'workers',
 
     # Our App
     'roamboter',
