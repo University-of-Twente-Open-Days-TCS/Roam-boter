2d2e4805c7ba773290eab508bfad06146eb52869
Jelle Meijer <jellejanwil@gmail.com>
2020-03-26 11:19:10 +0100

    Allow active AI to be set to null in admin

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -7,10 +7,13 @@ class WorkshopAdmin(admin.ModelAdmin):
     readonly_fields = ('creation_date',)
     list_display = ('id', 'creation_date', 'workshop_open',)
 
+
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('id', 'team_code', 'team_name', 'workshop')
+    fields = ('team_code', 'team_name', 'workshop', 'active_ai')
 
 
 admin.site.register(Workshop, WorkshopAdmin)
 admin.site.register(Team, TeamAdmin)
+
 admin.site.register(UserSession)
