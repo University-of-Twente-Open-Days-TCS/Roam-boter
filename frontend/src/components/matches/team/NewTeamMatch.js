c1f717d226cb8f2149d97a5fda1aec31c63240d4
s1729306 <j.j.spijker@student.utwente.nl>
2020-03-26 11:37:38 +0100

    Added snackbars for NewTeamMatch feedback and edited existing snackbars to match the style of AIEditor snackbars

--- frontend/src/components/matches/team/NewTeamMatch.js
+++ frontend/src/components/matches/team/NewTeamMatch.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
-import { Typography, Grid, Button } from '@material-ui/core'
+import {Typography, Grid, Button, Snackbar} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 import ContentBox from '../../layout/ContentBox'
 import SelectAIDialog from '../SelectAIDialog'
 
 import RoamBotAPI from '../../../RoamBotAPI'
+import Alert from "@material-ui/lab/Alert";
 
 
 const useStyles = makeStyles(theme => ({
@@ -29,6 +30,11 @@ const NewTeamMatch = props => {
     let [ais, setAis] = useState(null)
     let [selectAIOpen, setSelectAIOpen] = useState(false)
     let [selectedAI, setSelectedAI] = useState(null)
+    let [snackbar, setSnackbar] = useState({
+        open: false,
+        error: false,
+        message: ""
+    })
 
     useEffect(() => {
         async function updateAis() {
@@ -49,16 +55,28 @@ const NewTeamMatch = props => {
             if (prompt){
                 let call = await RoamBotAPI.putActiveAI(selectedAI.pk)
                 if(!call.ok){
-                    window.alert("Could not set as active AI")
+                    setSnackbar({
+                        message: "Could not set active AI",
+                        error: true,
+                        open: true
+                    })
                 }
             }
         }
 
         let call = await RoamBotAPI.postTeamMatch({gamemode: "DM", ai: selectedAI.pk})
         if (call.ok){
-            window.alert("Simulation successfull")
+            setSnackbar({
+                message: "Simulation successful! Match available under Replays",
+                error: false,
+                open: true
+            })
         }else {
-            window.alert("Match could not be simulation")
+            setSnackbar({
+                message: "Something went wrong, perhaps there are no other teams with an active AI yet?",
+                error: true,
+                open: true
+            })
         }
     }
 
@@ -85,6 +103,10 @@ const NewTeamMatch = props => {
                         <Button className={classes.button} type="submit" variant="contained" disabled={!(selectedAI)} color="secondary" onClick={playMatch}>Play</Button>
 
                         {(ais) ? (<SelectAIDialog ais={ais} open={selectAIOpen} handleClose={() => setSelectAIOpen(false)} handleClick={handleAIListItemClick} />) : "no AIs found"}
+
+                        <Snackbar open={snackbar.open} autoHideDuration={6000} onClose={() => setSnackbar({...snackbar, open: false})}>
+                            <Alert severity={snackbar.error ? "error" : "success"} elevation={6} variant="filled"> {snackbar.message} </Alert>
+                        </Snackbar>
                     </div>
                 </Grid>
             </Grid>
