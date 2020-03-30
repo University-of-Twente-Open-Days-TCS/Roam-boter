a50120830b87ad01c86fb89513825c3105b240ef
Daan Middelkoop <daantje676@gmail.com>
2020-03-30 11:36:31 +0200

    Possible TODO has been discussed and will not be improved

--- backend/simulation/bullet.py
+++ backend/simulation/bullet.py
@@ -40,9 +40,7 @@ class Bullet:
             return True
         return False
 
-
     # Check if a bullet has a collision with a tank.
-    # TODO: This is now a rectangle collision, do we want it to stay that way?
     def check_collision_tank(self, state):
         for tank in state.tanks:
             if tank == self.owner:
