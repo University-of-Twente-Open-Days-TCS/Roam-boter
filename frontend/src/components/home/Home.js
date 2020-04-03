822d86cf122ac971e9926deb82952ab16e91b75f
Jelle Meijer <jellejanwil@gmail.com>
2020-04-03 12:20:50 +0200

    Added tab layout

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,16 +1,48 @@
 import React, { useState, useEffect } from "react";
 
-import { Grid, Button } from '@material-ui/core'
+import SwipeableViews  from 'react-swipeable-views'
 
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Tabs, Tab, Button } from '@material-ui/core'
+
+
+
+import TeamInfo from './TeamInfo'
 import ContentBox from '../layout/ContentBox'
-
 import RoamBotAPI from '../../RoamBotAPI'
-import TeamInfo from "./TeamInfo";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+    },
+    swipeAbleViews: {
+        height: 'auto',
+        flexGrow: 1
+    },
+}))
+
+const TabPanel = props => {
+    const classes = useStyles()
+
+    return (
+        <div
+            className={classes.tabPanel}
+            role="tabpanel"
+            hidden={props.value !== props.index}
+        >
+            {props.children}
+        </div>
+    )
+}
 
 const Home = (props) => {
+    const classes = useStyles()
 
     let logoutHandler = props.handleSubmitLogout
 
+    const [tabIndex, setTabIndex] = useState(0)
     const [team, setTeam] = useState(null)
     const [ais, setAis] = useState(null)
 
@@ -59,13 +91,50 @@ const Home = (props) => {
         })
     }
 
+    const handleTabChange = (event, newValue) => setTabIndex(newValue)
+    const handleTabChangeIndex = index => setTabIndex(index)
+
     return (
-        <ContentBox>
-            {team ? <TeamInfo team={team} ais={ais} setActiveAI={setActiveAI}></TeamInfo> : null}
-            <Grid container justify="center" style={{marginTop: '1rem'}}>
-                <Button onClick={logoutHandler} variant="contained" color="secondary" style={{margin: '0.5rem'}}>Logout</Button>
-            </Grid>
-        </ContentBox>
+        <div className={classes.root}>
+            <AppBar position='static' color="transparent">
+                <Tabs 
+                    value={tabIndex}
+                    onChange={handleTabChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="scrollable"
+                    aria-label="tutorial tabs">
+                        <Tab label="Home" />
+                        <Tab label="Tab 2" />
+                        <Tab label="Tab 3" />
+                </Tabs>
+            </AppBar>
+
+            <SwipeableViews 
+                axis="x"
+                index={tabIndex}
+                onChangeIndex={handleTabChangeIndex}
+                className={classes.swipeAbleViews}
+            >
+                <TabPanel value={tabIndex} index={0}>
+                    <ContentBox>
+                        {team ? <TeamInfo team={team} ais={ais} setActiveAI={setActiveAI}/> : null}
+                        <div style={{margin: '0.5rem auto', textAlign: 'center'}}>
+                            <Button onClick={logoutHandler} variant="contained" color="secondary">Logout</Button>
+                        </div>
+                    </ContentBox>
+                </TabPanel>
+
+                <TabPanel value={tabIndex} index={1}>
+                    Item 2
+                </TabPanel>
+                <TabPanel value={tabIndex} index={2}>
+                    Item 3
+                </TabPanel>
+
+
+            </SwipeableViews>
+        </div>
     )
 }
 
