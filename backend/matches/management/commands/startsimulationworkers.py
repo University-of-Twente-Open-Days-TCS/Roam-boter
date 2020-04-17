bb2947b0b5224d85d79bea31772c89acb8ed1164
Jelle Meijer <jellejanwil@gmail.com>
2020-04-17 13:02:19 +0200

    Updated readme for the latest installation guide. Simulation worker prints an error if the database is not ready.

--- backend/matches/management/commands/startsimulationworkers.py
+++ backend/matches/management/commands/startsimulationworkers.py
@@ -1,16 +1,24 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import ProgrammingError
 
 from matches.simulation_workers import WorkerPool
+
+from matches.models import BotMatch, TeamMatch, Simulation
+from dashboard.models import Team
 
 
 class Command(BaseCommand):
 
-    help =  """
-            Start simulation workers to run simulations.\n
-            Note that this command will block.\n
-            You will have to call this command if you want to be able to simulate matches.
-            """
+    help = """
+           Start simulation workers to run simulations.\n
+           Note that this command will block.\n
+           You will have to call this command if you want to be able to simulate matches.
+           """
 
     def handle(self, *args, **options):
-        self.stdout.write("\u001b[35mStarting simulation workers\u001b[0m")
-        WorkerPool()
+        # check whether database is ready to start simulation workers
+        try:
+            self.stdout.write("\u001b[35mStarting simulation workers\u001b[0m")
+            WorkerPool()
+        except ProgrammingError:
+            self.stdout.write("\u001b[35mCould not start simulation workers, because database has not been initialized\u001b[0m")
